d31774964d95002b89be8e53b581eaae750b7903
yiminghe <yiminghe@gmail.com>
2023-04-04 12:22:07 +0800

    use beforeLoad

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,28 +1,43 @@
-import Garfish from 'garfish';
+import Garfish, { interfaces } from 'garfish';
 
 if (location.pathname === '/') {
   location.href = '/app1';
 }
 
+// must use extends
+interface ExtendAppInfo extends interfaces.AppInfo {
+  entryFn: () => string;
+}
+
+const apps: ExtendAppInfo[] = [
+  {
+    name: 'app1',
+    activeWhen: '/app1',
+    cache: false,
+    entry: 'http://x.com', // must be a url
+    entryFn() {
+      return `http://localhost:3001${location.pathname}${location.search}`;
+    },
+  },
+  {
+    name: 'app2',
+    activeWhen: '/app2',
+    cache: false,
+    entry: 'http://x.com', // must be a url
+    entryFn() {
+      return `http://localhost:3002${location.pathname}${location.search}`;
+    },
+  },
+];
+
 Garfish.run({
   basename: '/',
   domGetter: '#subApp',
-  apps: [
-    {
-      name: 'app1',
-      activeWhen: '/app1',
-      cache: false,
-      entry() {
-        return `http://localhost:3001${location.pathname}${location.search}`;
-      },
-    },
-    {
-      name: 'app2',
-      activeWhen: '/app2',
-      cache: false,
-      entry() {
-        return `http://localhost:3002${location.pathname}${location.search}`;
-      },
-    },
-  ] as any,
+  apps,
+  beforeLoad(appInfo: any) {
+    // any??
+    if (appInfo.entryFn) {
+      appInfo.entry = appInfo.entryFn();
+    }
+  },
 });
